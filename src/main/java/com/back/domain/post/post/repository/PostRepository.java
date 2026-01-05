78116639106ba72ed1e546e0e9d34daedabfd463
chan99k <kjkj5868@gmail.com>
2026-01-05 15:31:08 +0900

    Post 전체 조회 기능 구현 (Read - List)

--- src/main/java/com/back/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.java
@@ -1,8 +1,25 @@
 package com.back.domain.post.post.repository;
 
+import java.util.List;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import com.back.domain.post.post.document.Post;
 
-public interface PostRepository extends ElasticsearchRepository<Post,String> {
+public interface PostRepository extends ElasticsearchRepository<Post, String> {
+	/**
+	 * 왜 findAll()을 재선언하나요?
+	 * <ul>
+	 *     <li>
+	 *          ElasticsearchRepository의 기본 findAll()은 Iterable<Post>를 반환합니다.
+	 *     </li>
+	 *     <li>
+	 *         List<Post> 반환 타입으로 재선언하면 Spring Data가 자동으로 List로 변환해줍니다.
+	 *     </li>
+	 *     <li>
+	 *         JPA의 JpaRepository는 기본으로 List<T>를 반환하므로 재선언이 필요 없습니다.
+	 *     </li>
+	 * </ul>
+	 */
+	List<Post> findAll();
 }
--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -1,5 +1,7 @@
 package com.back.domain.post.post.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.back.domain.post.post.document.Post;
@@ -19,5 +21,9 @@ public class PostService {
 	public Post create(String title, String content, String author) {
 		Post post = new Post(title, content, author);
 		return postRepository.save(post);
+	}
+
+	public List<Post> findAll() {
+		return postRepository.findAll();
 	}
 }
