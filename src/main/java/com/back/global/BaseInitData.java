ccd70ccf8aa2cb9207c8f1af3a8150c0b65b2e21
chan99k <kjkj5868@gmail.com>
2026-01-05 15:07:02 +0900

    PostService 생성 및 count 메서드 구현

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -4,16 +4,27 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.back.domain.post.post.service.PostService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration // @Configuration 어노테이션으로 설정 클래스 지정
+@RequiredArgsConstructor
 public class BaseInitData {
+
+	private final PostService postService;
 
 	@Bean
 	public ApplicationRunner baseInitDataRunner() { // ApplicationRunner Bean을 통해 애플리케이션 시작 시 초기화 로직 실행
 		return args -> {
-			log.debug("ApplicationRunner 빈은 스프링에 등록되면 자동으로 실행됩니다");
+			// log.debug("ApplicationRunner 빈은 스프링에 등록되면 자동으로 실행됩니다");
+			work1();
 		};
+	}
+
+	private void work1() {
+		log.debug("Post entity 개수: {}", postService.count());
 	}
 }
